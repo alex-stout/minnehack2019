c7157c74d46ccee37c8625533488dabd193f4368
Rachel Korkowski <rachel.korkowski@gmail.com>
2019-02-03 06:04:50 -0600

    Updated routes

--- transaction-frontend/src/App.js
+++ transaction-frontend/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import TransactionForm from "./TransactionForm/TransactionForm";
 import TransactionConfirm from "./TransactionConfirm/TransactionConfirm";
+import CertTable from "./CertTable/CertTable";
+import RatingForm from "./RatingForm/RatingForm";
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/certification" exact component={CertTable} />
+        <Route path="/rate" exact component={RatingForm} />
         <Route path="/transaction" exact component={TransactionForm} />
         <Route path="/transactionConfirm" component={TransactionConfirm} />
       </Switch>
