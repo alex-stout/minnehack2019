ca8794285cfa09afaf096b1cbe47f059d0bf8ab0
Rachel Korkowski <rachel.korkowski@gmail.com>
2019-02-02 22:07:30 -0600

    Added html form

---
+++ transaction-frontend/src/RatingForm/RatingForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './RatingForm.css';
+
+class RatingForm extends Component {
+	render() {
+		return (
+			<form action="" method="get">
+				<table>
+					<tr>
+						<td>Mill Name:</td>
+						<td><input type="text" name="name"></input></td>
+					</tr><br />
+					<tr>
+						<td>Mill GPS Coordinates:</td>
+						<td></td>
+					</tr><br />
+					<tr>
+						<td>Roundtable on Sustainable Palm Oil (RSPO) Certification?</td>
+						<td>
+							<input type="radio" name="RSPO" value="yes"></input>Yes&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type="radio" name="RSPO" value="no"></input>No
+						</td>
+					</tr><br />
+					<tr>
+						<td>Involved in engagement/outreach programs?</td>
+						<td>
+							<input type="radio" name="programs" value="yes"></input>Yes&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type="radio" name="programs" value="no"></input>No
+							<br />Please Specify:<br />
+							<input type="text" name="programs"></input>
+						</td>
+					</tr><br />
+					<tr>
+						<td>Responsible Sourcing Policy?</td>
+						<td>
+							<input type="radio" name="sourcingPolicy" value="yes"></input>Yes&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type="radio" name="sourcingPolicy" value="no"></input>No
+							<br />Include Policy Here:<br />
+							<input type="text" name="sourcingPolicyText"></input>
+						</td>
+					</tr><br />
+					<tr>
+						<td>No Deforestation Policy?</td>
+						<td>
+							<input type="radio" name="deforestationPolicy" value="yes"></input>Yes&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type="radio" name="deforestationPolicy" value="no"></input>No
+							<br />Include Policy Here:<br />
+							<input type="text" name="deforestationPolicyText"></input>
+						</td>
+					</tr><br />
+					<tr>
+						<td>Average Employee Wage (USD):</td>
+						<td><input type="text" name="averageWage"></input></td>
+					</tr><br />
+					<tr>
+						<td></td>
+						<td>
+							<button type="submit">Submit</button>
+							<button type="reset">Reset</button>
+						</td>
+					</tr><br />
+				</table>
+			</form >
+		);
+	}
+}
+
+export default RatingForm;
